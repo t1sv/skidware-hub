40ea9ca0fdee366db3b6484c5beaf8aa066c0c4d
t1sv <88454783+t1sv@users.noreply.github.com>
2025-04-24 19:21:13 -0700

    vc

---
+++ scripts/vcn.lua
@@ -0,0 +1,4 @@
+-- This just re-enables voice chat if it got disconnected, do NOT use this more than 1-2 times
+-- If you're just trying to repeatedly curse, and bypass it, Roblox will ban you!
+
+game:GetService("VoiceChatService"):joinVoice()
